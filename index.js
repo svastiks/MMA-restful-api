9de4c888d70bb99793ec79210720c84d47704451
Svastik Sharma <svastiksharma13@gmail.com>
2023-11-01 23:41:49 -0400

    example code

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ const port = 3000;
 
 //routes
 
+app.get("/users", (req, res) => {
+
+    const users = [
+        {
+            id: 1,
+            name: "Simon",
+        },
+        {
+            id: 2,
+            name: "Brett",
+        },
+        {
+            id: 3,
+            name: "Random",
+        },
+    ];
+
+    res.json(users);
+
+});
+
 //start
 
 app.listen(port, () => {
