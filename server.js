b9c730dd2aa445029c01ceeb3bf2d2257646f1d4
Svastik Sharma <svastiksharma13@gmail.com>
2023-11-03 00:23:19 -0400

    small update

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/fighters', (req, res) =>{
     res.send("Hello API, yes");
 })
 
+app.get('/names', (req, res) =>{
+    res.send("Names");
+})
+
 app.listen(3000, () => {
     console.log("UFC API is running on port 3000");
 })
